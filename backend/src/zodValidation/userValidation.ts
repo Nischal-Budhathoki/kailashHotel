ff52243d0fd3dc83f44c61d7d9f6607938a84362
Nischal Budhathoki <bnischal91@gmail.com>
2026-05-23 10:13:28 +0400

    user validation updated

--- backend/src/zodValidation/userValidation.ts
+++ backend/src/zodValidation/userValidation.ts
@@ -4,7 +4,6 @@ import { z } from "zod"
 export const userRoleEnum = z.enum(["ADMIN", "USER"]);
 
 export const createUserSchema = z.object({
-  id: z.string().transform((val) => Number(val)),
   email: z
     .string()
     .email("Invalid email format")
